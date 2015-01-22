6c9ca5bd168610e507a13034e9a144750ae8bf34
Clara <quixoticduck@gmail.com>
2015-01-22 00:38:09 +0000

    Copied old Owl Carousel notes onto new working
    js file

--- html/js/myowlscript.js
+++ html/js/myowlscript.js
@@ -23,3 +23,14 @@ $(document).ready(function() {
    
 });
 
+//  navigation : true, // Show next and prev buttons
+//  pagination: true,
+//  items: 1,
+//  autoPlay : true,
+
+// "singleItem:true" is a shortcut for:
+// items : 1,
+// itemsDesktop : false,
+// itemsDesktopSmall : false,
+// itemsTablet: false,
+// itemsMobile : false
